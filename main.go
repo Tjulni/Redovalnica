6b7627e401635955e3220b5c210c5a3ec7e1a4c7
Tjulni <59819721+Tjulni@users.noreply.github.com>
2025-11-23 17:15:14 +0100

    initial commit

---
+++ main.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"log"
+
+	"github.com/urfave/cli/v3"
+)
+
+var stOcen = 6
+var minOcena = 0
+var maxOcena = 10
+
+func main() {
+	cmd := &cli.Command{
+		Name: "redovalnica",
+		Usage: "Upravljanje redovalnice študentov",
+		Flags: []cli.Flag{
+			&cli.IntFlag{
+				Name: "stOcen",
+				Usage: "Najmanjše število ocen potrebnih za pozitivno oceno",
+			},
+			&cli.IntFlag{
+				Name: "minOcena",
+				Usage: "Najmanjša možna ocena",
+			},
+			&cli.IntFlag{
+				Name: "maxOcena",
+				Usage: "Največja možna ocena",
+			},
+		},
+
+		Action: func(ctx context.Context, cmd *cli.Command) error {
+			return nil
+		},
+	}
+
+	if err := cmd.Run(context.Background(), os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
+
+
+type Student struct {
+	ime     string
+	priimek string
+	ocene   []int
+}
+
+func DodajOceno(studenti map[string]Student, vpisnaStevilka string, ocena int) {
+	st, obstaja := studenti[vpisnaStevilka]
+	if !obstaja {
+		fmt.Println("Napaka: študent z vpisno številko", vpisnaStevilka, "ne obstaja.")
+		return
+	}
+
+	if ocena < minOcena || ocena > maxOcena {
+		fmt.Printf("Napaka: ocena mora biti med %d in %d.", minOcena, maxOcena)
+		return
+	}
+
+	st.ocene = append(st.ocene, ocena)
+	studenti[vpisnaStevilka] = st
+}
+
+func povprecje(studenti map[string]Student, vpisnaStevilka string) float64 {
+	st, obstaja := studenti[vpisnaStevilka]
+	if !obstaja {
+		return -1.0
+	}
+
+	if len(st.ocene) < stOcen {
+		return 0
+	}
+
+	var vsota int
+	for _, ocena := range st.ocene {
+		vsota += ocena
+	}
+	return float64(vsota) / float64(len(st.ocene))
+}
+
+func IzpisVsehOcen(studenti map[string]Student) {
+	fmt.Println("REDOVALNICA:")
+	for vpisna, st := range studenti {
+		fmt.Printf("%s - %s %s: %v\n", vpisna, st.ime, st.priimek, st.ocene)
+	}
+}
+func IzpisiKoncniUspeh(studenti map[string]Student) {
+	for vpisna, student := range studenti {
+		p := povprecje(studenti, vpisna)
+		fmt.Printf("%s %s: povprečna ocena %.1f -> ", student.ime, student.priimek, p)
+
+		if p >= 9 {
+			fmt.Println("Odličen študent!")
+		} else if p >= 6 {
+			fmt.Println("Povprečen študent")
+		} else {
+			fmt.Println("Neuspešen študent")
+		}
+	}
+}
