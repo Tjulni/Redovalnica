0fc8d403b639ea411337868e3636f87bbb8c07f7
Tjulni <59819721+Tjulni@users.noreply.github.com>
2025-11-23 17:21:17 +0100

    small fix

--- redovalnica/redovalnica.go
+++ redovalnica/redovalnica.go
@@ -1,47 +1,12 @@
-package main
+package redovalnica
 
 import (
-	"context"
 	"fmt"
-	"os"
-	"log"
-
-	"github.com/urfave/cli/v3"
 )
 
 var stOcen = 6
 var minOcena = 0
 var maxOcena = 10
-
-func main() {
-	cmd := &cli.Command{
-		Name: "redovalnica",
-		Usage: "Upravljanje redovalnice študentov",
-		Flags: []cli.Flag{
-			&cli.IntFlag{
-				Name: "stOcen",
-				Usage: "Najmanjše število ocen potrebnih za pozitivno oceno",
-			},
-			&cli.IntFlag{
-				Name: "minOcena",
-				Usage: "Najmanjša možna ocena",
-			},
-			&cli.IntFlag{
-				Name: "maxOcena",
-				Usage: "Največja možna ocena",
-			},
-		},
-
-		Action: func(ctx context.Context, cmd *cli.Command) error {
-			return nil
-		},
-	}
-
-	if err := cmd.Run(context.Background(), os.Args); err != nil {
-		log.Fatal(err)
-	}
-}
-
 
 type Student struct {
 	ime     string
